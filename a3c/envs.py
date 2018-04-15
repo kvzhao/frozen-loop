7dba6cb9e7e3edd0473739af7c3e80c22e50a696
kvzhao <kelispinor@gmail.com>
2018-04-16 00:21:42 +0800

    Env sets settings in the folder

--- a3c/envs.py
+++ a3c/envs.py
@@ -14,7 +14,13 @@ def create_icegame_env(path, ID):
     else:
         print ("Env {} is not suitable for this project.".format(ID))
     env = gym.make(ID)
+    if not os.path.exists(path):
+        os.mkdir(path)
     env.set_output_path(path)
+    # save the settings
+    env.dump_env_setting()
+    # back up the initial configuration
+    env.save_ice()
     return env
 
-create_icegame_env(".", "IcegameEnv-v0")
+create_icegame_env("envLogs", "IcegameEnv-v0")
