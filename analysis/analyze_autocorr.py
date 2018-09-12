76ec1bf413796d114574b45f0ceeb261143b7d78
kvzhao <kelispinor@gmail.com>
2018-09-12 23:25:52 +0800

    acorr function

--- analysis/analyze_autocorr.py
+++ analysis/analyze_autocorr.py
@@ -13,6 +13,14 @@ def inner_prod(x, y):
     return 0
   return sum(i[0] * i[1] for i in zip(x, y))
 
+def integrated_autocorr_time(acorr):
+  N = len(acorr)
+  tau = 0
+  for t, c in enumerate(acorr):
+    tau += (1 - t/N) * c
+  tau = 2 * tau + 1
+  return tau
+
 def main():
 
   data = LogData(args.logdir, args.output)
@@ -21,17 +29,28 @@ def main():
   model = IceModel()
   model.set_ice(s0)
 
-  dots = []
+  observs = []
+
+  data.set_end_index(100)
 
   # you had better cal defect den as they do.
   for i, loop in enumerate(data.loops):
     model.apply_loop(loop)
     st = model.get_ice()
-    dot = inner_prod(st, s0)
-    dots.append(dot)
+    ##dd = inner_prod(st, s0)
+    dd = model.get_symm_vertex()
+    observs.append(dd)
 
-  obs = np.asarray(dots)
-  data.cal_autocorr(obs, "auto_corr")
+  obs = np.asarray(observs)
+  acorr = data.cal_autocorr(obs, "auto_corr")
+
+  #tau = integrated_autocorr_time(acorr)
+  #print ("Integrated autocorrelation time: {}".format(tau))
+
+  import acor
+  tau, mean, sigma = acor.acor(obs)
+  print ("Acor: tau = {}, mean = {}, sigma = {}".format(tau, mean, sigma))
+
 
 
 if __name__ == '__main__':
